c7c452dd1fbc13843105b0fd93f0c64dd37f5738
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-05-22 21:19:56 +0300

    Hello World Smart Contract

---
+++ hello-wold.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract PanosMatsGetResult {
+    constructor() {
+    }
+
+    function getResult() public pure returns(string memory) {
+        return "Hello World";
+    }    
+}
