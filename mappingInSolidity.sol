aef800ecf43a06ffb38d811e0ed91db5f36e81dc
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-02 11:34:02 +0300

    Mapping

---
+++ mappingInSolidity.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+contract LearnMapping {
+    mapping(address => uint) public myMap;
+
+    function getAddress(address _addr) public view returns(uint) {
+        return myMap[_addr];
+    }
+
+    function setAddress(address _addr, uint _i) public {
+        myMap[_addr] = _i;
+    }
+
+    function removeAddress(address _addr) public {
+        delete myMap[_addr];
+    }
+}
