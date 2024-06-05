51aebb0058664d7c8e02f2a990ad764473f8bd80
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-05 20:57:38 +0300

    Abstract Contracts

--- abstractContract.sol
+++ abstractContract.sol
@@ -10,3 +10,20 @@ contract Y is X {
         return "ehllo";
     }
 }
+
+abstract contract Member {
+    string name;
+    uint age = 38;
+
+    function setName() virtual public pure returns(string memory);
+
+    function returnAge() public view returns(uint) {
+        return age;
+    }
+}
+
+contract Teacher is Member {
+    function setName() override public pure returns(string memory) {
+        return "hello";
+    }
+}
