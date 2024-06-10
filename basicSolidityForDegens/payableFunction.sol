1b61985d610ba5a9044179ab137d87e1d23516d8
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-10 21:07:17 +0300

    Payable function

---
+++ basicSolidityForDegens/payableFunction.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+contract PaybableFunction {
+    function x() public payable {
+        // this function, if called will take the incoming Ether and save it to the contract balance.
+    }
+}
