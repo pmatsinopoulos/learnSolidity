8e53f1b495282537146873ab8d9bc51752447d32
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-10 17:28:16 +0200

    Check for addition overflow

--- baseTutorials/contracts/BasicMath.sol
+++ baseTutorials/contracts/BasicMath.sol
@@ -4,7 +4,12 @@ pragma solidity 0.8.26;
 contract BasicMath {
   function adder(uint _a, uint _b) external pure returns(uint sum, bool error) {
     uint result = 0;
-    result = _a + _b;
-    return (result, false);
+    unchecked {
+      result = _a + _b;
+      if (result >= _a) {
+        return (result, false);
+      }
+      return (0, true);
+    }
   }
 }
--- baseTutorials/test/contracts/BasicMath.ts
+++ baseTutorials/test/contracts/BasicMath.ts
@@ -12,9 +12,27 @@ describe("#adder", function () {
 
   it("adds the two numbers given and returns their sum and 'false' as error", async function () {
     const { basicMath } = await loadFixture(deployBasicMathFixture);
+
     const a = 1;
     const b = 2;
+
     const [result, error] = await basicMath.adder(a, b);
+
     expect(result).to.equal(3);
+    expect(error).to.equal(false);
+  });
+
+  context("when addition overflows", async function () {
+    it("returns 0 and error equal to 'true'", async function () {
+      const { basicMath } = await loadFixture(deployBasicMathFixture);
+
+      const a = hre.ethers.MaxUint256;
+      const b = 1;
+
+      const [result, error] = await basicMath.adder(a, b);
+
+      expect(result).to.equal(0);
+      expect(error).to.equal(true);
+    });
   });
 });
